a77e660994b88c165fa32ee7b90498e1ca89db3d
Danny Lee <Danny Lee@LAPTOP-P6VIH0R2.sduhsd.lan>
2018-02-08 14:39:18 -0800

    unit 4

---
+++ APCSA/src/unit4/Name.java
@@ -0,0 +1,40 @@
+package unit4;
+
+import static java.lang.System.*;
+
+public class Name
+{
+	private String name;
+
+	public Name()
+	{
+		setName(name);
+
+	}
+
+	public Name(String s)
+	{
+		name = s;
+
+	}
+
+   public void setName(String s)
+   {
+	   name = s;
+   }
+
+	public String getFirst()
+	{
+		return name.substring(0,name.indexOf(" "));
+	}
+
+	public String getLast()
+	{
+		return name.substring(name.indexOf(" ")+1, name.length());
+	}
+
+ 	public String toString()
+ 	{
+ 		return name + "\n";
+	}
+}
---
+++ APCSA/src/unit4/StringChecker.java
@@ -0,0 +1,54 @@
+package unit4;
+
+import static java.lang.System.*;
+
+public class StringChecker
+{
+	private String word;
+
+	public StringChecker()
+	{
+		setString(word);
+
+	}
+
+	public StringChecker(String s)
+	{
+		word = s;
+
+	}
+
+   public void setString(String s)
+   {
+   		word=s;
+   }
+
+	public boolean findLetter(char c)
+	{
+		if (-1 != word.indexOf(c))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public boolean findSubString(String s)
+	{
+		if (-1 != word.indexOf(s))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+ 	public String toString()
+ 	{
+ 		return "\n\n";
+	}
+}
