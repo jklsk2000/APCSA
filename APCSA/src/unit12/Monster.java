3d29b44a54116bbfb1b0aefb48e74777716909da
leej1516 <leej1516@CA-SU-F106-22.sduhsd.lan>
2018-03-13 15:00:16 -0700

    unit12

---
+++ APCSA/src/unit12/Monster.java
@@ -0,0 +1,90 @@
+package unit12;
+
+import static java.lang.System.*;
+
+public class Monster implements Comparable
+{
+	private int myWeight;
+	private int myHeight;
+	private int myAge;
+
+	//write a default Constructor
+	public Monster()
+	{
+		myWeight = 0;
+		myHeight = 0;
+		myAge = 0;
+	}
+	//write an initialization constructor with an int parameter ht
+	public Monster (int ht)
+	{
+		myHeight = ht;
+	}
+	//write an initialization constructor with int parameters ht and wt
+	public Monster (int ht, int wt)
+	{
+		myWeight = wt;
+		myHeight = ht;
+	}
+	//write an initialization constructor with int parameters ht, wt, and age
+	public Monster (int ht, int wt, int age)
+	{
+		myAge = age;
+		myWeight = wt;
+		myHeight = ht;
+	}
+	//modifiers - write set methods for height, weight, and age
+	public void setHeight (int ht)
+	{
+		myHeight = ht;
+	}
+	public void setWeight (int wt)
+	{
+		myWeight = wt;
+	}
+	public void setAge (int age)
+	{
+		myAge = age;
+	}
+	//accessors - write get methods for height, weight, and age
+	public int getHeight()
+	{
+		return myHeight;
+	}
+	public int getWeight()
+	{
+		return myWeight;
+	}
+	public int getAge()
+	{
+		return myAge;
+	}
+	
+	
+	//creates a new copy of this Object
+	public Object clone()
+	{
+	   return new Monster();
+	}
+
+	public boolean equals( Object obj )
+	{
+
+
+		return false;
+	}
+
+	public int compareTo( Object obj )
+	{
+		Monster rhs = (Monster)obj;
+
+		return -1;
+	}
+
+	//write a toString() method
+	public String toString() 
+	{
+		return "";
+	}
+	
+}
