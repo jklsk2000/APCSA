839c1d93ab17df59e49013135b6c9bee24d20244
Danny Lee <Danny Lee@DANNY-PC.attlocal.net>
2018-03-20 16:53:11 -0700

    unit14 half done

---
+++ APCSA/src/unit14/AtCounter.java
@@ -0,0 +1,81 @@
+package unit14;
+
+import static java.lang.System.*;
+
+public class AtCounter
+{
+   private char[][] atMat;
+   private int atCount;
+   
+   private boolean[][] been;
+
+	public AtCounter()
+	{
+		atCount=0;
+		atMat = new char[][]{       {'@','-','@','-','-','@','-','@','@','@'},
+									{'@','@','@','-','@','@','-','@','-','@'},
+									{'-','-','-','-','-','-','-','@','@','@'},
+									{'-','@','@','@','@','@','-','@','-','@'},
+									{'-','@','-','@','-','@','-','@','-','@'},
+									{'@','@','@','@','@','@','-','@','@','@'},
+									{'-','@','-','@','-','@','-','-','-','@'},
+									{'-','@','@','@','-','@','-','-','-','-'},
+									{'-','@','-','@','-','@','-','@','@','@'},
+									{'-','@','@','@','@','@','-','@','@','@'}};
+		been = new boolean [10][10];
+	}
+
+	public boolean inside(int r, int c){ //inbounds or not
+		return (r >= 0) && (r <= 9) && (c >= 0) && (c <= 9);
+	}
+	
+	public boolean beenThere(int r, int c){ //visited squares
+		return been[r][c];
+	}
+	
+	public int countAts(int r, int c)
+	{
+
+		//add in recursive code to count up the # of @s connected
+		//start checking at spot [r,c]
+
+		if (!inside(r,c) || atMat[r][c] == '-'){
+			return 0;
+		}
+		
+		if (inside(r,c) && !beenThere(r,c)){
+			been[r][c] = true;
+			if (atMat[r][c] == '@'){
+				atCount++;
+				
+				if (inside(r+1,c)){
+					countAts(r+1,c);
+				}
+				if (inside(r-1,c)){
+					countAts(r-1,c);
+				}
+				if (inside(r,c+1)){
+					countAts(r,c+1);
+				}
+				if (inside(r,c-1)){
+					countAts(r,c-1);
+				}
+			}
+		}
+
+		return atCount;
+	}
+
+	public int getAtCount()
+	{
+		return atCount;
+	}
+
+	public String toString()
+	{
+		String output="";
+		output+=getAtCount()+" @s connected.";
+		atCount = 0;
+		return output;
+	}
+}
---
+++ APCSA/src/unit14/Lab21g.java
@@ -0,0 +1,37 @@
+package unit14;
+
+import static java.lang.System.*;
+
+public class Lab21g
+{
+	public static void main(String args[])
+	{
+		//instantiate an AtCounter
+		AtCounter atat = new AtCounter();
+		//test the code
+		int row = 0;
+		int col = 0;
+		atat.countAts(row, col);
+		System.out.println(row + " " + col + " " + "has " + atat.toString());
+		
+		row = 2;
+		col = 5;
+		atat.countAts(row, col);
+		System.out.println(row + " " + col + " " + "has " + atat.toString());
+		
+		row = 5;
+		col = 0;
+		atat.countAts(row, col);
+		System.out.println(row + " " + col + " " + "has " + atat.toString());
+		
+		row = 9;
+		col = 9;
+		atat.countAts(row, col);
+		System.out.println(row + " " + col + " " + "has " + atat.toString());
+		
+		row = 3;
+		col = 9;
+		atat.countAts(row, col);
+		System.out.println(row + " " + col + " " + "has " + atat.toString());
+	}
+}
