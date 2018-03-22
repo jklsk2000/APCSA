967e183ba9fad264633c1944d4287f0819b0aa8f
Danny Lee <Danny Lee@LAPTOP-P6VIH0R2.sduhsd.lan>
2018-03-22 08:28:49 -0700

    unit 14 done!

--- APCSA/src/unit14/Lab24d.java
+++ APCSA/src/unit14/Lab24d.java
@@ -10,6 +10,15 @@ public class Lab24d
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner (new File(System.getProperty("user.dir") + "//src//unit14//Lab24d.dat"));
-		TicTacToe ttt = new TicTacToe();
+		int number = file.nextInt();
+		file.nextLine();
+		
+		for (int i = 0; i < number; i++){
+			String xo = file.nextLine();
+			TicTacToe ttt = new TicTacToe(xo);
+//			ttt.getWinner();
+			System.out.println(ttt.toString());
+		}
+			
 	}
 }
--- APCSA/src/unit14/TicTacToe.java
+++ APCSA/src/unit14/TicTacToe.java
@@ -9,23 +9,85 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
-
+		mat = new char [0][0];
 	}
 
 	public TicTacToe(String game)
 	{
-
+		char[] xo = game.toCharArray();
+		
+		mat = new char [3][3];
+		
+		int count = 0;
+		for(int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++){
+				mat[i][j] = xo[count];
+				count++;
+			}
+		}
+		
+		for(int x = 0; x < 3; x++){
+			for (int y = 0; y < 3; y++){
+				System.out.print(mat[x][y]);
+			}
+			System.out.println();
+		}
 	}
 
 	public String getWinner()
 	{
-		return "";
+		String output = "";
+		if (horizontal()){
+			
+		}
+		else if (vertical()){
+			
+		}
+		else if (diagonal()){
+			
+		}
+		else{
+			output += "draw";
+		}
+		return output;
+	}
+	
+	public boolean horizontal(){
+		for (int i = 0; i < 3; i++){
+			if (mat[i][0] == mat [i][1] && mat[i][1] == mat[i][2]){
+				System.out.println(mat[i][0] + " won horizontally");
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean vertical(){
+		for (int i = 0; i < 3; i++){
+			if (mat[0][i] == mat [1][i] && mat[1][i] == mat[2][i]){
+				System.out.println(mat[0][i] + " won vertically");
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean diagonal(){
+		if (mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2]){
+			System.out.println(mat[0][0] + " won diagonally");
+			return true;
+		}
+		else if (mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0]){
+			System.out.println(mat[0][2] + " won diagonally");
+			return true;
+		}
+		return false;
 	}
 
 	public String toString()
 	{
 		String output="";
-
+		output = getWinner();
 		return output+"\n\n";
 	}
 }
